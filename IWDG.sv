9b2030a9921f0a11e842e86de6e3733d8f615aee
VaporUser <VaporUser>
2018-12-09 16:42:12 +0100

    reorder the code layout

--- IWDG.sv
+++ IWDG.sv
@@ -1,71 +1,80 @@
 module IWDG 
 #(
 
-parameter GRL  = 1,  // Granularity of the data
+parameter GRL  = 1,                                 // Granularity of the data
 
-parameter IWDG_KR_SIZE   = 16,    // IWDG_KR  Last valid bit
-parameter IWDG_PR_SIZE   = 3,     // IWDG_PR  Last valid bit
-parameter IWDG_RLR_SIZE  = 12,    // IWDG_RLR Last valid bit
-parameter IWDG_ST_SIZE   = 2,     // IWDG_ST  Last valid bit
+parameter IWDG_KR_SIZE   = 16,                      // IWDG_KR  Last valid bit
+parameter IWDG_PR_SIZE   = 3,                       // IWDG_PR  Last valid bit
+parameter IWDG_RLR_SIZE  = 12,                      // IWDG_RLR Last valid bit
+parameter IWDG_ST_SIZE   = 2,                       // IWDG_ST  Last valid bit
 
 parameter BASE_ADR     = 32'h0100_0000,             // Memory base address of the registries.
 parameter IWDG_KR_ADR  = BASE_ADR + 32'h0000_0000,  // Memory address of IWDG_KR register
 parameter IWDG_PR_ADR  = BASE_ADR + 32'h0000_0004,  // Memory address of IWDG_PR register
 parameter IWDG_RLR_ADR = BASE_ADR + 32'h0000_0008,  // Memory address of IWDG_RLR register
-parameter IWDG_ST_ADR  = BASE_ADR + 32'h0000_000C  // Memory address of IWDG_ST register
+parameter IWDG_ST_ADR  = BASE_ADR + 32'h0000_000C   // Memory address of IWDG_ST register
 
 )
 (
-    input clk_lsi, // LSI clock from a RC oscillator 
+    input clk_lsi,              // LSI clock from a RC oscillator 
+      
+    input clk_m2s,              // Wishbone SYSCON module clock. The clock of the bus used to coordinate the activities on the interfaces
+    input rst_m2s,              // Wishbone SYSCON module reset signal. It forces the Wishbone interface to reset. ALL WISHBONE interfaces must have one.
     
-    input clk_m2s, // Wishbone SYSCON module clock. The clock of the bus used to coordinate the activities on the interfaces
-    input rst_m2s, // Wishbone SYSCON module reset signal. It forces the Wishbone interface to reset. ALL WISHBONE interfaces must have one.
-    
-    input [31:0] dat_m2s,  // Wishbone interface binary array used to pass data arriving from the bus.
-    input [31:0] adr_m2s,  // Wishbone interface binary array used to pass address arriving from the bus.
-    input [GRL:0] sel_m2s, // Wishbone interface signal used to indicate valid data on dat_m2s during READ or dat_s2m on WRITE cycle.
+    input [31:0] dat_m2s,       // Wishbone interface binary array used to pass data arriving from the bus.
+    input [31:0] adr_m2s,       // Wishbone interface binary array used to pass address arriving from the bus.
+    input [GRL:0] sel_m2s,      // Wishbone interface signal used to indicate valid data on dat_m2s during READ or dat_s2m on WRITE cycle.
         
-    input cyc_m2s,        // Wishbone interface signal which indicates that a valid bus cycle is in progress.
-    input we_m2s,         // Wishbone interface signal which indicates is a WRITE or READ bus cycle
+    input cyc_m2s,              // Wishbone interface signal which indicates that a valid bus cycle is in progress.
+    input we_m2s,               // Wishbone interface signal which indicates is a WRITE or READ bus cycle
     
-    input lok_m2s,        // Wishbone interface signal used to indicate that current bus cycle is uninterruptible
-    input stb_m2s,        // Wishbone interface signal used to qualify other signal in a valid cycle.
+    input lok_m2s,              // Wishbone interface signal used to indicate that current bus cycle is uninterruptible
+    input stb_m2s,              // Wishbone interface signal used to qualify other signal in a valid cycle.
 /*    
-    input tga_m2s,        // Wishbone address tag signal qualified by stb_m2s.    
-    input tgc_m2s,        // Wishbone cycle tag signal qualified by cyc_m2s.  
-    input tgd_m2s,        // Wishbone data tag signal qualified by stb_m2s.  
+    input tga_m2s,              // Wishbone address tag signal qualified by stb_m2s.    
+    input tgc_m2s,              // Wishbone cycle tag signal qualified by cyc_m2s.  
+    input tgd_m2s,              // Wishbone data tag signal qualified by stb_m2s.  
 */  
-    output err_s2m,       // Wishbone interface signal used by SLAVE to indicate an abnormal cycle termination.
-    output rty_s2m,       // Wishbone interface signal used by SLAVE to indicate that it is not ready to accept or send data.
+    output err_s2m,             // Wishbone interface signal used by SLAVE to indicate an abnormal cycle termination.
+    output rty_s2m,             // Wishbone interface signal used by SLAVE to indicate that it is not ready to accept or send data.
     
-    // output tgd_s2m,       // Wishbone data tag signal qualified by stb_m2s.
-    
+    // output tgd_s2m,          // Wishbone data tag signal qualified by stb_m2s. 
     output reg [31:0] dat_s2m,  // Wishbone interface binary array used to pass data to the bus.
     output reg ack_s2m,         // Wishbone interface signal used by the SLAVE to acknoledge a MASTER request. 
-    output rst_iwdg        // IWDG reset signal
+    
+    output rst_iwdg             // IWDG reset signal
 );
+                                                      // TOP FSA MOORE MACHINE: WISHBONE INTERFACE
+                                                      // - REGISTERS:
 
-reg [IWDG_KR_SIZE - 1:0]  iwdg_kr,  iwdg_kr_next;     // IWDG Key Register. Write here byte words to control the IWDG.
-reg [IWDG_PR_SIZE - 1:0]  iwdg_pr,  iwdg_pr_next;     // IWDG Prescale Register. It changes the LSI clock frequency. Initially WRITE PROTECTED
-reg [IWDG_RLR_SIZE- 1:0]  iwdg_rlr, iwdg_rlr_next;    // IWDG Reload Register. It stores the watchdog timer countdown. Initially WRITE PROTECTED
-reg [IWDG_ST_SIZE - 1:0]  iwdg_st,  iwdg_st_next;     // IWDG Status Register. It sets its bit when a prescale is done or an IWDG reset signal is emitted.
+reg [IWDG_KR_SIZE - 1:0]  iwdg_kr,  iwdg_kr_next;     // IWDG Key Register. Key word changes the state of LOWER FSA MOORE MACHINE.
+reg [IWDG_PR_SIZE - 1:0]  iwdg_pr,  iwdg_pr_next;     // IWDG Prescale Register. It changes how many clk cycle to count to change the countdown.
+reg [IWDG_RLR_SIZE- 1:0]  iwdg_rlr, iwdg_rlr_next;    // IWDG Reload Register. It stores the IWDG countdown.
+reg [IWDG_ST_SIZE - 1:0]  iwdg_st,  iwdg_st_next;     // IWDG Status Register. It is set when a RELOAD or PRESCALE operation is done.
 
-reg [1:0] s, ss_next;       // Moore state machine
+                                                      // - MOORE STATES:
+reg [1:0] s, ss_next;                                 // 3 states codified in 2 bits. One is unused.
 
-reg [IWDG_KR_SIZE - 1:0] t, tt_next;
-reg [IWDG_RLR_SIZE- 1:0] cnt_rlr, cnt_rlr_next;    
 
-reg sts_pr,  sts_pr_next;
-reg sts_rlr, sts_rlr_next;
+                                                      // LOWER FSA MOORE MACHINE: COUNTDOWN
+                                                      // - REGISTERS:
 
-reg [7:0] cnt_pr, cnt_pr_next;  // prescale divider
-reg [7:0] thr_pr, thr_pr_next;  // prescale divider
+reg [IWDG_RLR_SIZE- 1:0] cnt_rlr, cnt_rlr_next;       // RLR-Sized bit counter register. The downcount is performed here. 
+reg [7:0] cnt_pr, cnt_pr_next;                        // 8-bit counter register. Counts how many lsi-clock transition before donwcount the cnt_rlr
+reg [7:0] thr_pr, thr_pr_next;                        // 8-bit threshold register. Constant value needed to compare the cnt_pr register.
+reg sts_pr,  sts_pr_next;                             // 1-bit signal. Is set when a PRESCALE operation is done.
+reg sts_rlr, sts_rlr_next;                            // 1-bit signal. Is set when a RELOAD operation is done
+
+                                                      // - MOORE STATES:
+reg [IWDG_KR_SIZE - 1:0] t, tt_next;                  // the state is equal to IWDG Key Register.
+
 
 reg rst_iwdg_tmp;
-
 wire read_cyc, write_cyc;
 
-localparam PR_0 = 3'b000;
+                                                      // PARAMETERS:
+                                                      // - PRESCALE TRUTH TABLE
+localparam PR_0 = 3'b000;                             
 localparam PR_1 = 3'b001;
 localparam PR_2 = 3'b010;
 localparam PR_3 = 3'b011;
@@ -81,15 +90,19 @@ localparam PR_DIV_64  =  9'b0_0100_0000 - 1;
 localparam PR_DIV_128 =  9'b0_1000_0000 - 1;
 localparam PR_DIV_256 =  9'b1_0000_0000 - 1;
 
-localparam IDLE  = 2'b00;   // IDLE  state
-localparam READ  = 2'b01;   // READ  state
-localparam WRITE = 2'b10;   // WRITE state
+                                                    // - TOP FSA MOORE MACHINE states decodification
+localparam IDLE  = 2'b00;   
+localparam READ  = 2'b01;   
+localparam WRITE = 2'b10;   
 
+                                                    // - LOWER FSA MOORE MACHINE states decodification
 localparam IDLE_KEY   = 16'h0000;
-localparam COUNT_KEY  = 16'hCCCC; // start countdown  IWDG_KR value
-localparam RELOAD_KEY = 16'hAAAA; // 
-localparam ACCESS_KEY = 16'h5555; // 
+localparam COUNT_KEY  = 16'hCCCC; 
+localparam RELOAD_KEY = 16'hAAAA;  
+localparam ACCESS_KEY = 16'h5555;  
 
+
+                                                    // TOP FSA MOORE: Sequential part 
 always @(posedge clk_m2s)
 begin
     if (rst_m2s)               
@@ -113,8 +126,8 @@ begin
 
         end
 end
-
-always @(posedge clk_lsi)
+                                                    // LOWER FSA MOORE: Sequential part
+always @(posedge clk_lsi)                            
 begin    
     if (rst_m2s) 
         begin
@@ -136,6 +149,50 @@ begin
         end
 end
 
+                                                    // TOP FSA MOORE: Combinatorial part
+always @(*)
+begin
+    ss_next = s;
+    iwdg_kr_next  = iwdg_kr;
+    iwdg_rlr_next = iwdg_rlr;
+    iwdg_pr_next  = iwdg_pr;   
+    iwdg_st_next  = iwdg_st;
+    
+    case (s)
+        IDLE: 
+            begin 
+                if(read_cyc) ss_next = READ;
+                else if (write_cyc) ss_next = WRITE;
+                ack_s2m = 0;
+            end
+        READ:
+            begin
+                case(adr_m2s)
+                    IWDG_KR_ADR:    dat_s2m = iwdg_kr_next;
+                    IWDG_PR_ADR:    dat_s2m = iwdg_pr_next;  
+                    IWDG_RLR_ADR:   dat_s2m = iwdg_rlr_next; 
+                    IWDG_ST_ADR:    dat_s2m = iwdg_st_next;
+                endcase
+                ack_s2m = cyc_m2s & stb_m2s;
+                ss_next = IDLE;
+            end 
+        WRITE:
+            begin
+                case(adr_m2s)
+                    IWDG_KR_ADR:    iwdg_kr_next  = dat_m2s;
+                    IWDG_PR_ADR:    iwdg_pr_next  = dat_m2s;  
+                    IWDG_RLR_ADR:   iwdg_rlr_next = dat_m2s; 
+                    IWDG_ST_ADR:    iwdg_st_next  = dat_m2s;
+                endcase         
+                ack_s2m = cyc_m2s & stb_m2s;
+                ss_next = IDLE;
+            end
+        default: 
+            ss_next = IDLE;        
+    endcase
+end
+
+                                                    // LOWER FSA MOORE: Combinatorial part
 always @(*)
 begin
     tt_next = iwdg_kr_next;    
@@ -184,48 +241,6 @@ begin
                     PR_6: thr_pr_next = PR_DIV_256;
                 endcase
             end      
-    endcase
-end
-
-always @(*)
-begin
-    ss_next = s;
-    iwdg_kr_next  = iwdg_kr;
-    iwdg_rlr_next = iwdg_rlr;
-    iwdg_pr_next  = iwdg_pr;   
-    iwdg_st_next  = iwdg_st;
-    
-    case (s)
-        IDLE: 
-            begin 
-                if(read_cyc) ss_next = READ;
-                else if (write_cyc) ss_next = WRITE;
-                ack_s2m = 0;
-            end
-        READ:
-            begin
-                case(adr_m2s)
-                    IWDG_KR_ADR:    dat_s2m = iwdg_kr_next;
-                    IWDG_PR_ADR:    dat_s2m = iwdg_pr_next;  
-                    IWDG_RLR_ADR:   dat_s2m = iwdg_rlr_next; 
-                    IWDG_ST_ADR:    dat_s2m = iwdg_st_next;
-                endcase
-                ack_s2m = cyc_m2s & stb_m2s;
-                ss_next = IDLE;
-            end 
-        WRITE:
-            begin
-                case(adr_m2s)
-                    IWDG_KR_ADR:    iwdg_kr_next  = dat_m2s;
-                    IWDG_PR_ADR:    iwdg_pr_next  = dat_m2s;  
-                    IWDG_RLR_ADR:   iwdg_rlr_next = dat_m2s; 
-                    IWDG_ST_ADR:    iwdg_st_next  = dat_m2s;
-                endcase         
-                ack_s2m = cyc_m2s & stb_m2s;
-                ss_next = IDLE;
-            end
-        default: 
-            ss_next = IDLE;        
     endcase
 end
 
--- tb_IWDG.sv
+++ tb_IWDG.sv
@@ -4,21 +4,21 @@ module tb_IWDG
 #(
 
 localparam CLK_PERIOD = 10,
-localparam CLK_LSI_PERIOD = 20, // 31.25 us for 32kHz rc oscillator
+localparam CLK_LSI_PERIOD = 20, 
 localparam END_TEST = 400,
 
-localparam GRL  = 1,  // Granularity of the data    
+localparam GRL  = 1,      
 
-localparam IWDG_KR_SIZE   = 16,    // IWDG_KR  Last valid bit
-localparam IWDG_PR_SIZE   = 3,     // IWDG_PR  Last valid bit
-localparam IWDG_RLR_SIZE  = 12,    // IWDG_RLR Last valid bit
-localparam IWDG_ST_SIZE   = 2,      // IWDG_ST  Last valid bit
+localparam IWDG_KR_SIZE   = 16,    
+localparam IWDG_PR_SIZE   = 3,     
+localparam IWDG_RLR_SIZE  = 12,    
+localparam IWDG_ST_SIZE   = 2,     
 
-localparam BASE_ADR     = 32'h0100_0000,             // Memory base address of the registries.
-localparam IWDG_KR_ADR  = BASE_ADR + 32'h0000_0000,  // Memory address of IWDG_KR register
-localparam IWDG_PR_ADR  = BASE_ADR + 32'h0000_0004,  // Memory address of IWDG_PR register
-localparam IWDG_RLR_ADR = BASE_ADR + 32'h0000_0008,  // Memory address of IWDG_RLR register
-localparam IWDG_ST_ADR  = BASE_ADR + 32'h0000_000C  // Memory address of IWDG_ST register
+localparam BASE_ADR     = 32'h0100_0000,             
+localparam IWDG_KR_ADR  = BASE_ADR + 32'h0000_0000,  
+localparam IWDG_PR_ADR  = BASE_ADR + 32'h0000_0004,  
+localparam IWDG_RLR_ADR = BASE_ADR + 32'h0000_0008,  
+localparam IWDG_ST_ADR  = BASE_ADR + 32'h0000_000C  
 
 )();
 
@@ -33,31 +33,31 @@ reg [GRL:0] sel_m2s;
 reg cyc_m2s;
 reg we_m2s;    
 
-reg lok_m2s;        // Wishbone interface signal used to indicate that current bus cycle is uninterruptible
-reg stb_m2s;        // Wishbone interface signal used to qualify other signal in a valid cycle.
+reg lok_m2s;                // Wishbone interface signal used to indicate that current bus cycle is uninterruptible
+reg stb_m2s;                // Wishbone interface signal used to qualify other signal in a valid cycle.
 
 /*
-reg tga_m2s;        // Wishbone address tag signal qualified by stb_m2s.    
-reg tgc_m2s;        // Wishbone cycle tag signal qualified by cyc_m2s.  
-reg tgd_m2s;        // Wishbone data tag signal qualified by stb_m2s.  
+reg tga_m2s;                // Wishbone address tag signal qualified by stb_m2s.    
+reg tgc_m2s;                // Wishbone cycle tag signal qualified by cyc_m2s.  
+reg tgd_m2s;                // Wishbone data tag signal qualified by stb_m2s.  
 */
 
-reg err_s2m;       // Wishbone interface signal used by SLAVE to indicate an abnormal cycle termination.
-reg rty_s2m;       // Wishbone interface signal used by SLAVE to indicate that it is not ready to accept or send data.
+reg err_s2m;                // Wishbone interface signal used by SLAVE to indicate an abnormal cycle termination.
+reg rty_s2m;                // Wishbone interface signal used by SLAVE to indicate that it is not ready to accept or send data.
 
-// reg tgd_s2m,       // Wishbone data tag signal qualified by stb_m2s.
+// reg tgd_s2m,             // Wishbone data tag signal qualified by stb_m2s.
 
-reg [31:0] dat_s2m;  // Wishbone interface binary array used to pass data to the bus.
-reg ack_s2m;         // Wishbone interface signal used by the SLAVE to acknoledge a MASTER request. 
-reg rst_iwdg;        // IWDG reset signal
+reg [31:0] dat_s2m;         // Wishbone interface binary array used to pass data to the bus.
+reg ack_s2m;                // Wishbone interface signal used by the SLAVE to acknoledge a MASTER request. 
+reg rst_iwdg;               // IWDG reset signal
 
 
-always #(CLK_PERIOD / 2) clk_m2s = ~clk_m2s;   // clock generation
-always #(CLK_LSI_PERIOD / 2) clk_lsi = ~clk_lsi;   // clock generation
+always #(CLK_PERIOD / 2) clk_m2s = ~clk_m2s;                    // clock generation
+always #(CLK_LSI_PERIOD / 2) clk_lsi = ~clk_lsi;                // clock generation
 
-IWDG   // IWDG instance
+IWDG                                                            // IWDG instance
  #(
-    .GRL(GRL),  // Granularity of the data
+    .GRL(GRL),                                                  // Granularity of the data
 
     .IWDG_KR_SIZE(IWDG_KR_SIZE),    
     .IWDG_PR_SIZE(IWDG_PR_SIZE),     
@@ -72,25 +72,25 @@ IWDG   // IWDG instance
 
 ) IWDG (
     .clk_lsi(clk_lsi),
-    .clk_m2s(clk_m2s), // Wishbone SYSCON module clock. The clock of the bus used to coordinate the activities on the interfaces
-    .rst_m2s(rst_m2s), // Wishbone SYSCON module reset signal. It forces the Wishbone interface to reset. ALL WISHBONE interfaces must have one.
+    .clk_m2s(clk_m2s), 
+    .rst_m2s(rst_m2s), 
     
-    .dat_m2s(dat_m2s), // Wishbone interface binary array used to pass data arriving from the bus.
-    .adr_m2s(adr_m2s), // Wishbone interface binary array used to pass address arriving from the bus.
+    .dat_m2s(dat_m2s), 
+    .adr_m2s(adr_m2s), 
     .sel_m2s(sel_m2s),
     
-    .cyc_m2s(cyc_m2s), // Wishbone interface signal which indicates that a valid bus cycle is in progress.
-    .we_m2s(we_m2s),   // Wishbone interface signal which indicates is a WRITE or READ bus cycle
+    .cyc_m2s(cyc_m2s), 
+    .we_m2s(we_m2s),   
     
     .lok_m2s(lok_m2s),    
-    .stb_m2s(stb_m2s), // Wishbone interface signal which is used by the MASTER to poll the SLAVE.
+    .stb_m2s(stb_m2s), 
     
     .err_s2m(err_s2m),
     .rty_s2m(rty_s2m),
     
-    .dat_s2m(dat_s2m),   // Wishbone interface binary array used to pass data to the bus.
-    .ack_s2m(ack_s2m),   // Wishbone interface signal used by the SLAVE to acknoledge a MASTER request. 
-    .rst_iwdg(rst_iwdg)  // IWDG reset signal
+    .dat_s2m(dat_s2m), 
+    .ack_s2m(ack_s2m), 
+    .rst_iwdg(rst_iwdg)
     
 );
 
@@ -100,8 +100,8 @@ initial
         clk_lsi <= 0; 
         rst_m2s = 1;    
                 
-        cyc_m2s = 0;    // must be 0 when rst is 1    
-        stb_m2s = 0;    // must be 0 when rst is 1
+        cyc_m2s = 0;        
+        stb_m2s = 0;    
                         
         repeat(2) @(posedge clk_m2s);
         rst_m2s <= 0;         
